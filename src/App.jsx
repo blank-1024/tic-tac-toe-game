eef33d124f676bdc4d3a511906be8fa9502ca693
blank-1024 <13380497335@163.com>
2022-06-16 00:55:25 +0800

    commit my update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import React from "react";
 import Game from "./components/Game";
 
-function App() {
+export default function App() {
   return (
     <div>
-      <Game></Game>
+      <Game/>
     </div>
   );
 }
-
-export default App;
